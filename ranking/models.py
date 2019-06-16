80ab5dfca13b7acd4fb397d0b8c554e99852842f
Tomasz Twardoch <ttwardoch3@gmail.com>
2019-06-16 16:20:00 +0200

    ranking_app_update

--- ranking/models.py
+++ ranking/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from main.models import Project
 
-# Create your models here.
+class Rating(models.Model):
+
+    rvalues = ((1,1),
+              (2,2),
+              (3,3),
+              (4,4),
+              (5,5))
+
+    rating = models.PositiveSmallIntegerField(choices=rvalues)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE) (import user)
+
+    def __str__(self):
+        return str(self.rating)
--- ranking/views.py
+++ ranking/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from main.models import Project
 
 class RankingView(View):
-    pass
+
+    def get(self, request):
+        top_projects = Project.objects.all().order_by('-average_rating')[:10]
+        return render(request, 'ranking/ranking.html', {'projects': top_projects})
