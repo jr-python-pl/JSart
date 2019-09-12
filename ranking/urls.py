8757c9ff5c642421b9f22ca63dadefbda5336edf
Mateusz Hryciuk <mateusz.hryciuk@outlook.com>
2019-09-13 00:17:53 +0200

    profile edit repair,translation repair,housekeeping

--- ranking/urls.py
+++ ranking/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import RankingView
-from django.utils.translation import ugettext_lazy as _
+
 
 app_name = 'ranking'
 urlpatterns = [
-    path(_('ranking/'), RankingView.as_view(), name="ranking"),
+    path('ranking/', RankingView.as_view(), name="ranking"),
 
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
