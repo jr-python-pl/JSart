7e01586e716b906e63d5da0713e98836be4f9a0e
Mateusz Hryciuk <mateusz.hryciuk@outlook.com>
2019-09-12 10:10:26 +0200

    some translation fixes

--- main/urls.py
+++ main/urls.py
@@ -2,23 +2,24 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from main.views import Home, ContactEmail, AboutView, PortfolioView, ProjectView, AddProjectView, AuthorsView, SuccessView,RunProjectScript
+from django.utils.translation import ugettext_lazy as _
 
 
 app_name = 'main'
 urlpatterns = [
 
-    path('main/', Home.as_view(), name="home"),
-    path('project/<int:id>', ProjectView.as_view(), name="project_view"),
-    path('run_project/<int:id>', RunProjectScript.as_view(), name="runscript"),
-    path('portfolio/', PortfolioView.as_view(), name="portfolio"),
-    path('about/', AboutView.as_view(), name="about"),
-    path('contact/', ContactEmail.as_view(), name="contact"),
-    path('success/', SuccessView.as_view(), name='success'),
-    path('add_project/', AddProjectView.as_view(), name="add_project"),
-
+    path(_('main/'), Home.as_view(), name="home"),
+    path(_('project/<int:id>'), ProjectView.as_view(), name="project_view"),
+    path(_('run_project/<int:id>'), RunProjectScript.as_view(), name="runscript"),
+    path(_('projects/'), PortfolioView.as_view(), name="portfolio"),
+    path(_('about/'), AboutView.as_view(), name="about"),
+    path(_('contact/'), ContactEmail.as_view(), name="contact"),
+    path(_('success/'), SuccessView.as_view(), name='success'),
+    path(_('add_project/'), AddProjectView.as_view(), name="add_project"),
+    
     #authors
-    path('authors/', AuthorsView.as_view(), name="authors"),
-
+    path(_('authors/'), AuthorsView.as_view(), name="authors"),
+    # urls translation with ugettext_lazy as __ doesn't work i will leave it as it is now (app works just fine)-Mateusz- 
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
